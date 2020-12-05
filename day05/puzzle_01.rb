4ba4cf18e064884b4688cda3bb464f91a0deee0c
Vy Dao <vynguyendao@gmail.com>
2020-12-05 22:44:15 +0700

    Day 05 - puzzle 01,02

---
+++ day05/puzzle_01.rb
@@ -0,0 +1,26 @@
+def seat_ids
+  data = read_file
+  data.map do |seat|
+    rows = (0..127).to_a
+    columns = (0..8).to_a
+    seat.chars.each do |char|
+      case char
+      when 'F'
+        rows = rows.slice(0, rows.length / 2)
+      when 'B'
+        rows = rows.slice(rows.length / 2, rows.length)
+      when 'L'
+        columns = columns.slice(0, columns.length / 2)
+      when 'R'
+        columns = columns.slice(columns.length / 2, columns.length)
+      end
+    end
+    rows.pop * 8 + columns.pop
+  end
+end
+
+def read_file
+  File.open('seats.txt').readlines.map(&:chomp)
+end
+
+puts seat_ids.max
